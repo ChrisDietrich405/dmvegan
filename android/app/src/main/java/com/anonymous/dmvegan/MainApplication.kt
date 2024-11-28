0b97ad8615e369573d48f78407ced0244cfc3d7f
Chris <chrisdietrich3666@gmail.com>
2024-11-27 20:10:15 -0500

    test

--- android/app/src/main/java/com/anonymous/dmvegan/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/dmvegan/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.dmvegan
+package com.anonymous.dmvegan
 
 import android.app.Application
 import android.content.res.Configuration
--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -8,12 +8,14 @@ import {
 	statusCodes,
 } from "@react-native-google-signin/google-signin";
 
+import { WEB_CLIENT_ID, IOS_CLIENT_ID } from "../keys/index";
+
 GoogleSignin.configure({
 	webClientId: WEB_CLIENT_ID, // client ID of type WEB for your server. Required to get the `idToken` on the user object, and for offline access.
 	scopes: ["https://www.googleapis.com/auth/drive.readonly"], // what API you want to access on behalf of the user, default is email and profile
 	offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
 	forceCodeForRefreshToken: false, // [Android] related to `serverAuthCode`, read the docs link below *.
-	iosClientId: "<FROM DEVELOPER CONSOLE>", // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+	iosClientId: IOS_CLIENT_ID, // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
 });
 
 export default function LoginScreen() {
@@ -131,7 +133,9 @@ export default function LoginScreen() {
 			<GoogleSigninButton
 				size={GoogleSigninButton.Size.Wide}
 				color={GoogleSigninButton.Color.Dark}
-			></GoogleSigninButton>
+			>
+				<h1>Hello</h1>
+			</GoogleSigninButton>
 
 			<Button
 				title="Baltimore"
